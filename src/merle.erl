d01330ce338c7e911559670e9be93d1d636b769f
zeusfaber <joe@joetify.com>
2009-01-18 21:16:30 -0800

    added src file attribution

--- src/merle.erl
+++ src/merle.erl
@@ -1,4 +1,5 @@
-%% Copyright (c) 2009 Joseph Williams <joe@joetify.com>
+%% Copyright 2009, Joe Williams <joe@joetify.com>
+%% Copyright 2009, Nick Gerakines <nick@gerakines.net>
 %%
 %% Permission is hereby granted, free of charge, to any person
 %% obtaining a copy of this software and associated documentation
